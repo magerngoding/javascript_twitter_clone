c0d8677e2ce0634512a9119d0a154419ba986b51
Hafizh <Hafizh@gmail.com>
2025-03-25 10:41:41 +0700

    Membuat fitur hapus tweet, detail profil user

--- assets/js/userProfile.js
+++ assets/js/userProfile.js
@@ -1,32 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // Penting pertama deklarasi 
-    const usernameLoggedIn = localStorage.getItem('usernameLoggedIn')
+    const usernameLoggedIn = localStorage.getItem('usernameLoggedIn');
+    const usernameProfileChosen = localStorage.getItem('usernameProfileChosen');
 
-    const instantFeedback = document.getElementById('instantFeedback');
-    instantFeedback.style.display = 'none';
-
-    const twittForm = document.getElementById('twittForm');
     const ownerPhoto = document.getElementById('ownerPhoto');
     const twittsWrapper = document.getElementById('twittsWrapper');
-    const twittContent = document.getElementById('twittContent');
-
-    let selectedFeeling = null;
-
-    // select semua class
-    const feeligItems = document.querySelectorAll('.item-feeling')
-
-    feeligItems.forEach(item => {
-
-        item.addEventListener('click', () => {
-
-            selectedFeeling = item.getAttribute('data-feeling') // attribute div
-
-            feeligItems.forEach(i => i.classList.remove('border-[#1880e8]'));
-
-            item.classList.add('border-[#1880e8]');
-        })
-    });
+    const userProfileName = document.getElementById('userProfileName');
+    const userProfileUsername = document.getElementById('userProfileUsername');
 
     // import model
     const twittManager = new Twitt();
@@ -34,50 +15,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const twittUsers = userManager.getUsers();
 
+    const userProfileChosen = twittUsers.find(user => user.username.toLowerCase() === usernameProfileChosen.toLowerCase());
 
-    const ownerLoggedIn = twittUsers.find(user => user.username.toLowerCase() === usernameLoggedIn.toLowerCase());
-    ownerPhoto.src = ownerLoggedIn.avatar;
+    //  const ownerLoggedIn = twittUsers.find(user => user.username.toLowerCase() === usernameLoggedIn.toLowerCase());
 
-    // membuat format tanggal yang disimpan 'yyyy-mm-dd'
-    const now = new Date()
-    const year = now.getFullYear()
-    const month = String(now.getMonth() + 1).padStart(2, '0') // 01 02 03
-    const day = String(now.getDate()).padStart(2, '0')
-
-    twittForm.addEventListener('submit', (event) => {
-
-        event.preventDefault(); // agar tidak scroll ke atas
-
-        const twittData = { // object data yang akan dilempar
-            twittContent: twittContent.value,
-            twittUsernameOwner: usernameLoggedIn,
-            twittFeeling: selectedFeeling,
-            twittCreatedAt: `${year}-${month}-${day}`,
-        };
-
-        const result = twittManager.saveTwitt(twittData);
-
-        if (result.success) {
-            // reset data form kembali kosong 
-            instantFeedback.style.display = 'none';
-            twittContent.value = '';
-            selectedFeeling = null;
-
-            feeligItems.forEach(item => {
-                item.classList.remove('border-[#1880e8]');
-            });
-
-            // cara refresh data twitts twitts = existingTwitts
-            displayAllTwitts(twittManager.getTwitts());
-        }
-        else {
-            instantFeedback.style.display = 'flex';
-            instantFeedback.textContent = result.error;
-        }
-    })
+    ownerPhoto.src = userProfileChosen.avatar;
+    userProfileName.textContent = userProfileChosen.name;
+    userProfileUsername.textContent = '@' + userProfileChosen.username;
 
     const existingTwitts = twittManager.getTwitts();
-    // cara refresh data twitts twitts = existingTwitts
     const existingLoveTwitts = twittManager.getLoveTwitts();
 
     function displayAllTwitts(twitts = existingTwitts) {
